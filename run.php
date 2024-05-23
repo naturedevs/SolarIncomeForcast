fffb8284308de4a8d681e530e98d7a46d9952e4f
naturedevs <naturesky0411@gmail.com>
2024-05-23 05:47:56 +0200

    1

--- run.php
+++ run.php
@@ -2,7 +2,7 @@
 use WHMCS\Database\Capsule;
 require_once ("Forecast.php");
 require_once (__DIR__."/../../../includes/index.php");
-require_once (__DIR__."/../../../sola88/addonmodules.php");
+// require_once (__DIR__."/../../../sola88/addonmodules.php");
 use WHMCS\Module\Addon\AddonModule\Forecast;
 echo "start";
 try {
